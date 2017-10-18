5422338710912af662df97e1638e734c41e55401
moeabdol <mohd.a.saed@gmail.com>
2017-10-18 23:39:16 +0300

    Install express ejs eslint

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Workding!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port', 3000);
+});
