9fa4e903d3e41040386a0decfc6ec48edc3e3697
moeabdol <mohd.a.saed@gmail.com>
2017-10-19 15:31:55 +0300

    Create page model

---
+++ models/page.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const pageSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  slug: { type: String },
+  content: { type: String, required: true },
+  sorting: { type: Number }
+});
+
+module.exports = mongoose.model('Page', pageSchema);
