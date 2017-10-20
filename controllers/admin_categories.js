d45b8b3fd6703e6002730b0f5c0c2d3c27162dfc
moeabdol <mohd.a.saed@gmail.com>
2017-10-20 19:13:45 +0300

    Implement POST /admin/categories/edit/:id

--- controllers/admin_categories.js
+++ controllers/admin_categories.js
@@ -56,8 +56,65 @@ const createCategory = (req, res) => {
   });
 };
 
+const editCategory = (req, res) => {
+  Category.findById(req.params.id, (err, category) => {
+    if (err) return console.log(err);
+
+    res.render('admin/categories/edit', {
+      id: category._id,
+      title: category.title
+    });
+  });
+};
+
+const updateCategory = (req, res) => {
+  req.checkBody('title', 'Title must have a value.').notEmpty();
+
+  let title = req.body.title;
+  let slug = title.replace(/\s+/g, '-').toLowerCase();
+  let id = req.params.id;
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    return res.render('admin/categories/edit', {
+      errors: errors,
+      title: title,
+      id: id
+    });
+  }
+
+  Category.findOne({ slug: slug, _id: { '$ne': id } }, (err, category) => {
+    if (err) return console.log(err);
+
+    if (category) {
+      req.flash('danger', 'Category title exists, choose another.');
+      return res.render('admin/categories/edit', {
+        id: id,
+        title: title
+      });
+    }
+
+    Category.findById(id, (err, category) => {
+      if (err) return console.log(err);
+
+      category.title = title;
+      category.slug = slug;
+
+      category.save(err => {
+        if (err) return console.log(err);
+
+        req.flash('success', 'Category edited.');
+        res.redirect('/admin/categories');
+      });
+    });
+  });
+};
+
 module.exports = {
   index,
   newCategory,
-  createCategory
+  createCategory,
+  editCategory,
+  updateCategory
 };
--- routes/admin_categories.js
+++ routes/admin_categories.js
@@ -5,5 +5,7 @@ const adminCategories = require('../controllers/admin_categories');
 router.get('/', adminCategories.index);
 router.get('/new', adminCategories.newCategory);
 router.post('/new', adminCategories.createCategory);
+router.get('/edit/:id', adminCategories.editCategory);
+router.post('/edit/:id', adminCategories.updateCategory);
 
 module.exports = router;
