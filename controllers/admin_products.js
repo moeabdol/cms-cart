c5020c8adcfe158c5887ace9e8f6f06794bd4cf9
moeabdol <mohd.a.saed@gmail.com>
2017-10-20 23:21:48 +0300

    Implement GET /admin/products

--- controllers/admin_products.js
+++ controllers/admin_products.js
@@ -1,4 +1,27 @@
-const Product = require('../models/product');
-const mkdirp = require('mkdirp');
-const fs = require('fs-extra');
+const Product   = require('../models/product');
+const Category  = require('../models/category');
+const mkdirp    = require('mkdirp');
+const fs        = require('fs-extra');
 const resizeImg = require('resize-img');
+
+const index = (req, res) => {
+  let count;
+
+  Product.count((err, c) => {
+    if (err) return console.log(err);
+    count = c;
+
+    Product.find((err, products) => {
+      if (err) return console.log(err);
+
+      res.render('admin/products', {
+        products: products,
+        count: count
+      });
+    });
+  });
+};
+
+module.exports = {
+  index
+};
--- routes/admin_products.js
+++ routes/admin_products.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const adminProducts = require('../controllers/admin_products');
 
+router.get('/', adminProducts.index);
+
 module.exports = router;
